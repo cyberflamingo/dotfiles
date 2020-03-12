f7ff39bb7f502ee483295a6f61dcd051e33f6b00
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-03-12 22:58:44 +0900

    Add zsh install and p10k config

---
+++ install-zsh.sh
@@ -0,0 +1,25 @@
+# First let's install ZSH
+sudo apt-get install zsh
+
+# Make zsh default shell
+chsh -s $(which zsh)
+exec $SHELL
+
+# Install recommended font for Powerlevel10k
+# TODO: Check if ~/.fonts exist before mkdir-ing it
+FONTS=$HOME"/.fonts"
+FONTS_REPO="https://github.com/romkatv/powerlevel10k-media/raw/master"
+
+mkdir $FONTS
+wget $FONTS_REPO/MesloLGS%20NF%20Regular.ttf -P $FONTS
+wget $FONTS_REPO/MesloLGS%20NF%20Bold.ttf -P $FONTS
+wget $FONTS_REPO/MesloLGS%20NF%20Italic.ttf -P $FONTS
+wget $FONTS_REPO/MesloLGS%20NF%20Bold%20Italic.ttf -P $FONTS
+
+# Update font cache
+fc-cache -f -v
+
+# Add font to terminal
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "MesloLGS NF Regular"
+
+exec $SHELL
