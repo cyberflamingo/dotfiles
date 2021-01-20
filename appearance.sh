ec7eb2451648582650caab1ef972c863279a1a9e
Alexandre Mercier <alex@cyberflamingo.net>
2021-01-20 14:27:47 +0900

    Rewrite appearance bootstrap script
    - Rewrite variables and delete unused one
    - Remove Papirus icon and folder, including dependencies
    - Dont sudo script downloaded from the web
    - Remove rm theme folders
    - Update gsettings key
    - Change name of dumped terminal conf just in case

--- appearance.sh
+++ appearance.sh
@@ -17,19 +17,17 @@ echo 'Applying appearance settings...'
 # Variables                                                                   #
 ###############################################################################
 
-THEME_APP_DIR=$HOME/Matcha-gtk-theme
-THEME_CUR_DIR=$HOME/Qogir-icon-theme
-ICO_DEST_DIR="/usr/share/icons"
+REPOSITORY_DIR=$HOME/Development/src/github.com/vinceliuice
+THEME_APP_DIR=$REPOSITORY_DIR/Matcha-gtk-theme
+THEME_CUR_DIR=$REPOSITORY_DIR/Qogir-icon-theme
 
 
 ###############################################################################
 # Dependencies                                                                #
 ###############################################################################
 
-sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
-sudo apt-get -y install gtk2-engines-murrine gtk2-engines-pixbuf \
-  papirus-icon-theme papirus-folders libreoffice-style-papirus
+sudo apt-get -y install gtk2-engines-murrine gtk2-engines-pixbuf
 
 
 ###############################################################################
@@ -38,13 +36,11 @@ sudo apt-get -y install gtk2-engines-murrine gtk2-engines-pixbuf \
 
 git clone https://github.com/vinceliuice/Matcha-gtk-theme.git "$THEME_APP_DIR"
 
-sudo bash "$THEME_APP_DIR"/install.sh --color dark --theme aliz
+bash "$THEME_APP_DIR"/install.sh --color dark --theme aliz
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Matcha-dark-aliz'
 
 flatpak install -y flathub org.gtk.Gtk3theme.Matcha-dark-azul
-
-rm -rf "$THEME_APP_DIR"
 
 
 ###############################################################################
@@ -53,28 +49,22 @@ rm -rf "$THEME_APP_DIR"
 
 git clone https://github.com/vinceliuice/Qogir-icon-theme.git "$THEME_CUR_DIR"
 
-sudo cp -r "$THEME_CUR_DIR"/src/cursors/dist-ubuntu/ \
-  $ICO_DEST_DIR/Qogir-ubuntu-cursors
+bash "$THEME_CUR_DIR"/install.sh
 
-gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-ubuntu-cursors'
-
-rm -rf "$THEME_CUR_DIR"
+gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-ubuntu'
 
 
 ###############################################################################
 # Themes: Icons                                                               #
 ###############################################################################
 
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-
-papirus-folders -C red --theme Papirus-Dark
-
+gsettings set org.gnome.desktop.interface icon-theme 'Qogir-ubuntu'
 
 ###############################################################################
 # Themes: Shell                                                               #
 ###############################################################################
 
-gsettings set org.gnome.shell.extensions.user-theme name Pop-dark
+gsettings set org.gnome.shell.extensions.user-theme name 'Pop-dark'
 gsettings set org.gnome.shell.extensions.user-theme name 'Matcha-dark-aliz'
 
 
@@ -83,7 +73,7 @@ gsettings set org.gnome.shell.extensions.user-theme name 'Matcha-dark-aliz'
 ###############################################################################
 
 # Save with this command (replacing the string with whatever it is you have):
-# dconf dump /org/gnome/terminal/legacy/profiles:/:49cc915e-fb7c-4bfb-be99-f4839b4c63d2/ > gnome-terminal.dconf
+# dconf dump /org/gnome/terminal/legacy/profiles:/:49cc915e-fb7c-4bfb-be99-f4839b4c63d2/ > gnome-terminal.dconf.bak
 dconf load /org/gnome/terminal/legacy/profiles:/:49cc915e-fb7c-4bfb-be99-f4839b4c63d2/ < gnome-terminal.dconf
 
 echo 'Every tasks done.'
